721540cb0213a53d8ec9322eead85cfbbc971a50
Cheick Diaby <cheick.diaby@inetum.com>
2025-02-26 21:40:36 +0100

    Authetififcation keycloak

---
+++ AdminIvoire.WebApi/Authentication/ServicesExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AdminIvoire.WebApi.Authentication;
+
+public static class ServicesExtensions
+{
+    public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.Authority = configuration["Jwt:Authority"];
+            options.Audience = configuration["Jwt:Audience"];
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+    }
+}
